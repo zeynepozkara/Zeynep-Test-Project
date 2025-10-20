1d50e0fd8be4f9495cba99bc993de54def1e33e6
zeynepozkara <znurozk1@stu.vistula.edu.pl>
2025-10-20 17:18:39 +0200

    Add Hello Vistula

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class HelloWorld {
     public static void main(String[] args) {
-
-
-        for (int i = 1; i <= 5; i++) {//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-
-        }
+        System.out.println("Hello, Vistula!");
     }
 }
